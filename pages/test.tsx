9c668d6c64832a28b94915c5450e5342753bfd30
YiDaoJ <jcxsusan@gmail.com>
2022-10-24 11:54:43 +0200

    testpage: fetch data and render

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,7 +1,28 @@
-import type { NextPage } from "next";
+import type { InferGetStaticPropsType, NextPage } from "next";
+import { RadioButton, RadioGroup } from "../components";
 
-const Test: NextPage = () => {
-  return <div>Test</div>;
+// This gets called on every request
+export async function getStaticProps() {
+  // Fetch data from external API
+  const res = await fetch("http://localhost:4000/test");
+  const data = await res.json();
+
+  // Pass data to the page via props
+  return { props: { fruits: data.fruits } };
+}
+
+const Test: NextPage = ({
+  fruits,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const label = `Choose your favorite from the following ${fruits?.length} fruits：`;
+
+  return (
+    <RadioGroup name="fruit-selection" label={label} horizontal>
+      {fruits?.map((item) => (
+        <RadioButton value={item} label={item} key={item} />
+      ))}
+    </RadioGroup>
+  );
 };
 
 export default Test;
