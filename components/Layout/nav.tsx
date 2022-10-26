02e5598e7df49ec33d502e70fa33f4f2dd745cf2
YiDaoJ <jcxsusan@gmail.com>
2022-10-26 09:01:26 +0200

    styling selected nav item

--- components/Layout/nav.tsx
+++ components/Layout/nav.tsx
@@ -1,21 +1,26 @@
 import styled from "@emotion/styled";
 import Link from "next/link";
 import { FC } from "react";
+import { useRouter } from "next/router";
 
 export const Nav: FC = () => {
+  const router = useRouter();
+
+  const pathname = router.pathname;
+
   return (
     <StyledList>
-      <StyledItem>
+      <StyledItem selected={pathname === "/"}>
         <Link href="/">
           <a>Home</a>
         </Link>
       </StyledItem>
-      <StyledItem>
+      <StyledItem selected={pathname === "/fruit"}>
         <Link href="/fruit">
           <a>Fruit</a>
         </Link>
       </StyledItem>
-      <StyledItem>
+      <StyledItem selected={pathname === "/framework"}>
         <Link href="/framework">
           <a>Framework</a>
         </Link>
@@ -33,7 +38,7 @@ const StyledList = styled.ul`
   padding: 0;
 `;
 
-const StyledItem = styled.li`
+const StyledItem = styled.li<{ selected: boolean }>`
   list-style: none;
   display: inline-block;
   font-size: 1.1rem;
@@ -44,7 +49,7 @@ const StyledItem = styled.li`
   a {
     text-decoration: none;
     transition: all 0.3s ease;
-    color: #151515;
+    color: ${({ selected }) => (selected ? "#e00" : "#151515")};
 
     :hover {
       color: #e00;
